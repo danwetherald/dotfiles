4281e9bdc337ab1f59184e10177aad6a2060eefb
Daniel Wetherald <dan.wetherald@gmail.com>
2023-10-25 23:39:40 -0500

    snippets for rails

--- nvim_custom/init.lua
+++ nvim_custom/init.lua
@@ -1,7 +1,7 @@
 -- Close if only tree is open
 vim.api.nvim_create_autocmd("BufEnter", {
-  command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
-  nested = true,
+	command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+	nested = true,
 })
 
 vim.g.mapleader = ","
@@ -13,6 +13,11 @@ vim.opt.relativenumber = true
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.swapfile = false
--- vim.opt.updatecount = false
-
 vim.opt.backspace = { "indent", "eol,start" }
+
+-- Enable true colors
+if vim.opt.termguicolors then
+	vim.opt.termguicolors = true
+end
+
+vim.g.vscode_snippets_path = "~/.config/nvim/lua/custom/snippets/json"
--- nvim_custom/plugins.lua
+++ nvim_custom/plugins.lua
@@ -1,14 +1,11 @@
 local plugins = {
-  {
+	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			{
-				-- format & linting
-				"jose-elias-alvarez/null-ls.nvim",
-				config = function()
-					require("custom.configs.null-ls")
-				end,
-			},
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
 		},
 		config = function()
 			require("plugins.configs.lspconfig")
@@ -16,87 +13,86 @@ local plugins = {
 		end,
 	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- lua
-        "lua-language-server",
-        "stylua",
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- lua
+				"lua-language-server",
+				"stylua",
 
-        -- web
-        "css-lsp",
-        "html-lsp",
-        "tailwindcss-language-server",
+				-- web
+				"css-lsp",
+				"html-lsp",
+				"tailwindcss-language-server",
 
-        -- misc
-        "yaml-language-server",
+				-- misc
+				"yaml-language-server",
 
-        -- rails
-        "ruby-lsp",
-      }
-    }
-  },
+				-- rails
+				"ruby-lsp",
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "html",
-        "css",
-        "javascript",
-        "json",
-        "toml",
-        "lua",
-        "ruby",
-        "dockerfile",
-        "typescript",
-        "tsx",
-        "yaml",
-      }
-    }
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"html",
+				"css",
+				"javascript",
+				"json",
+				"toml",
+				"lua",
+				"ruby",
+				"dockerfile",
+				"typescript",
+				"tsx",
+				"yaml",
+			},
+		},
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      diagnostics = {
-        enable = true,
-      },
-      git = {
-        enable = true,
-        ignore = false,
-      },
-      actions = {
-        open_file = {
-          quit_on_open = true,
-          window_picker = {
-            enable = false
-          }
-        }
-      }
-    }
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			diagnostics = {
+				enable = true,
+			},
+			git = {
+				enable = true,
+				ignore = false,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+		},
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				local ls = require("luasnip")
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = {
-      filetype_exclude = {
-        "help",
-        "terminal",
-        "alpha",
-        "packer",
-        "lspinfo",
-        "TelescopePrompt",
-        "TelescopeResults",
-        "nvchad_cheatsheet",
-        "lsp-installer",
-        "norg",
-        "",
-      },
-    }
-  }
+				ls.filetype_extend("ruby", { "rails" })
+				ls.filetype_extend("eruby", { "html", "rails", "ruby" })
+			end,
+		},
+		opts = {
+			filetype_extend = {
+				"eruby",
+				"ruby",
+			},
+		},
+	},
 }
 
 return plugins
